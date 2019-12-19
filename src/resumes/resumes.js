d4a8cafc7239eba18d4aba53d0a42f0a99de541b
fengde <jun1st.feng@gmail.com>
2019-12-19 23:09:36 +0800

    Initial commit

---
+++ src/resumes/resumes.js
@@ -0,0 +1,10 @@
+// If you create a new resume, import it here:
+import './material-dark.vue';
+import './left-right.vue';
+import './oblique.vue';
+import './side-bar.vue';
+import './purple.vue';
+import './side-bar-rtl.vue';
+import './left-right-rtl.vue';
+import './creative.vue';
+import './cool.vue';
